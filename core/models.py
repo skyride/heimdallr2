12fb694b901656817710490d9129f477bde3a2d5
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-01-26 11:44:24 +0000

    Created alliance/corp/char models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Alliance(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=64, db_index=True)
+    ticker = models.CharField(max_length=5, db_index=True)
+    is_closed = models.BooleanField(default=False)
+
+    founded = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class Corporation(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=64, db_index=True)
+    ticker = models.CharField(max_length=5, db_index=True)
+    alliance = models.ForeignKey(Alliance, null=True, default=None, on_delete=models.SET_NULL)
+    is_closed = models.BooleanField(default=False)
+
+    founded = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class Character(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=64, db_index=True)
+    birthday = models.DateTimeField()

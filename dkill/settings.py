d5594405859f3dd2a373f8f0fe09fa2d65fd674e
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-01-26 11:30:59 +0000

    Added sde database entry

--- dkill/settings.py
+++ dkill/settings.py
@@ -69,6 +69,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'sde': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlite-latest.sqlite'),
     }
 }
 

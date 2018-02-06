72a6765a10b17b9b06dae5300370ba9b9261a517
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-06 12:10:39 +0000

    Fixed map url formats to work with old scheme

--- api/urls.py
+++ api/urls.py
@@ -22,9 +22,10 @@ urlpatterns = [
     re_path(r'autocomplete/character/(.+)', views.autocomplete_character, name="autocomplete_character"),
     re_path(r'autocomplete/corporation/(.+)', views.autocomplete_corporation, name="autocomplete_corporation"),
     re_path(r'autocomplete/alliance/(.+)', views.autocomplete_alliance, name="autocomplete_alliance"),
-    re_path(r'autocomplete/system/(.*)', views.autocomplete_system, name="autocomplete_system"),
-    re_path(r'autocomplete/constellation/(.*)', views.autocomplete_constellation, name="autocomplete_constellation"),
-    re_path(r'autocomplete/region/(.*)', views.autocomplete_region, name="autocomplete_region"),
-    re_path(r'autocomplete/ship/(.*)', views.autocomplete_ship, name="autocomplete_ship"),
+    re_path(r'autocomplete/ships/(.*)', views.autocomplete_ship, name="autocomplete_ship"),
     re_path(r'autocomplete/item/(.*)', views.autocomplete_item, name="autocomplete_item"),
+
+    re_path(r'map/systems/(.*)', views.autocomplete_system, name="autocomplete_system"),
+    re_path(r'map/constellations/(.*)', views.autocomplete_constellation, name="autocomplete_constellation"),
+    re_path(r'map/regions/(.*)', views.autocomplete_region, name="autocomplete_region"),
 ]

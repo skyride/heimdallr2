7eb93af6fefbe571efd780a4a377f3f4e542b423
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-06 14:39:47 +0000

    Fixed angular variable references in HTML

--- api/views/search.py
+++ api/views/search.py
@@ -1,7 +1,7 @@
 import ujson
 from base64 import b64decode
 
-from django.db.models import Q
+from django.db.models import Q, Count
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
@@ -27,6 +27,8 @@ def search(request, data):
 
     kms = Killmail.objects.filter(
         query
+    ).annotate(
+        involved_count=Count('involved')
     ).order_by(
         '-date'
     )[:10]
@@ -37,10 +39,22 @@ def search(request, data):
 
 def generate_json(kms):
     out = []
+
     for km in kms:
-        out.append({
+        o = {
             "id": km.id,
-            "date": km.date
-        })
+            "date": km.date.strftime("%Y-%m-%d %H:%m"),
+            "value": km.value,
+            "ship_id": km.ship_id,
+            "ship_name": km.ship.name,
+            "system_id": km.system_id,
+            "system_name": km.system.name,
+            "system_sec": km.system.security,
+            "region_id": km.system.region_id,
+            "region_name": km.system.region.name,
+            "attackers": km.involved_count - 1,
+            
+        }
+        out.append(o)
 
     return ujson.dumps(out)

e451368dbba1ed5a4d070a60b46ea83b8cc45782
Adam Findlay <skylinerspeeder@gmail.com>
2018-02-10 18:12:54 +0000

    Fixed bug in date

--- api/views/search.py
+++ api/views/search.py
@@ -45,7 +45,7 @@ def generate_json(kms):
     for km in kms[:50]:
         o = {
             "id": km.id,
-            "date": km.id.strftime("%Y-%m-%d %H:%m"),
+            "date": km.date.strftime("%Y-%m-%d %H:%m"),
             "value": km.value,
             "ship_id": km.ship_id,
             "ship_name": km.ship.name,

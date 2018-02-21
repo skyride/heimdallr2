5f4e5a27375adf213974cd61335a9a3e7c5cd67a
Adam Findlay <adam.findlay@mercurytide.co.uk>
2018-02-21 12:38:36 +0000

    Fixed another bug in ESI cost calculation

--- core/tasks.py
+++ core/tasks.py
@@ -402,8 +402,8 @@ def parse_esi(json=None, keyhash=None, attempts=0, source_id=3):
             )
         ).aggregate(
             total_value=Sum('value')
-        )['total_value'] + float(km.ship.sell)
-        if km.value != None:
+        )['total_value']
+        if km.value == None:
             km.value = float(km.ship.sell)
         else:
             km.value = km.value + float(km.ship.sell)
